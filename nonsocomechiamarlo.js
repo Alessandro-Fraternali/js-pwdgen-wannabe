4737d519f6b893e54533eab37a4238e949869420
Wandering Crow <FraternaliAlessandro@gmail.com>
2020-03-18 15:56:18 +0100

    Esercizio concluso

--- nonsocomechiamarlo.js
+++ nonsocomechiamarlo.js
@@ -1,7 +1,16 @@
 // chiedere nome
+var nome;
+nome = prompt("Come ti chiami?");
 
 // chiedere cognome
+var cognome;
+cognome = prompt("E il tuo cognome invece?");
 
 // chiedere colore preferito
+var colore;
+colore = prompt("Di che colore era il cavallo bianco di Napoleone?");
 
 // stampadati e 19
+password = nome + cognome + colore + 19;
+
+document.getElementById("javapower").innerHTML = password;
